a4deca3e63b0ce84043716e02f78a018a758130d
Vasily Shashov <shashovva@mail.ru>
2025-11-25 21:53:59 +0300

    Проект выполнен

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -8,12 +8,58 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var numberLabel: UILabel!
+    @IBOutlet weak var dataTextView: UITextView!
+    @IBOutlet weak var plusButton: UIButton!
+    @IBOutlet weak var minusButton: UIButton!
+    @IBOutlet weak var escButton: UIButton!
+    
+    private var number: Int = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        numberLabel.text = "\(number)"
+        dataTextView.text = "История изменений:"
+        dataTextView.isEditable = false
+        dataTextView.isScrollEnabled = true
     }
-
-
+    
+    private func formatDate(_ date: Date) -> String {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateStyle = .medium
+        dateFormatter.timeStyle = .medium
+        dateFormatter.locale = Locale(identifier: "ru_RU")
+        return dateFormatter.string(from: date)
+    }
+    
+    @IBAction func changePlusButton(_ sender: Any) {
+    number += 1
+        numberLabel.text = "\(number)"
+        let currentDate = Date()
+        dataTextView.text = "(\(formatDate(currentDate))\n Значение изменено на +1)"
+    }
+    
+    @IBAction func changeMinusButton(_ sender: Any) {
+        number -= 1
+        numberLabel.text = "\(number)"
+        if number < 0 {
+            number = 0
+            let currentDate = Date()
+            dataTextView.text = "(\(formatDate(currentDate))\n Попытка уменьшить значение счетчика ниже 0)"
+            self.numberLabel.text = "\(number)"
+        }
+        else{
+            let currentDate = Date()
+            dataTextView.text = "(\(formatDate(currentDate))\n Значение изменено на -1)"
+        }
+    }
+    
+    @IBAction func changeEscButton(_ sender: Any) {
+       number = 0
+        numberLabel.text = "\(number)"
+        let currentDate = Date()
+        dataTextView.text = "(\(formatDate(currentDate))\n Значение сброшено)"
+    }
 }
 
